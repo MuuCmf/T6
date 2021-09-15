5842bcd4bdafad80415741a5459cffce76120cad
dameng <59262424@qq.com>
2021-09-15 11:14:11 +0800

    腾讯云短信SDK

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,10 +8,10 @@ class ComposerStaticInitbde435928c820c82e3ad724d7f427ac5
 {
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        '9b552a3cc426e3287cc811caefa3cf53' => __DIR__ . '/..' . '/topthink/think-helper/src/helper.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
+        '9b552a3cc426e3287cc811caefa3cf53' => __DIR__ . '/..' . '/topthink/think-helper/src/helper.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
@@ -36,6 +36,10 @@ class ComposerStaticInitbde435928c820c82e3ad724d7f427ac5
         'a' => 
         array (
             'app\\' => 4,
+        ),
+        'T' => 
+        array (
+            'TencentCloud\\' => 13,
         ),
         'S' => 
         array (
@@ -107,6 +111,11 @@ class ComposerStaticInitbde435928c820c82e3ad724d7f427ac5
         'app\\' => 
         array (
             0 => __DIR__ . '/../..' . '/app',
+        ),
+        'TencentCloud\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/tencentcloud/common/src/TencentCloud',
+            1 => __DIR__ . '/..' . '/tencentcloud/sms/src/TencentCloud',
         ),
         'Symfony\\Polyfill\\Php80\\' => 
         array (
