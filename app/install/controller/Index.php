9d9a02452554738615b3d9bb945165228f2690a4
dameng <59262424@qq.com>
2021-09-16 16:21:22 +0800

    新增install模块

---
+++ app/install/controller/Index.php
@@ -0,0 +1,37 @@
+<?php
+namespace app\install\controller;
+
+use think\facade\Db;
+use think\facade\View;
+use app\common\controller\Base;
+
+class Index extends Base
+{
+    //安装首页
+    public function index(){
+
+        if (is_file(ROOT_PATH . 'install.lock'))
+        {
+            // 已经安装过了 执行更新程序
+            $msg = '请删除install.lock文件后再运行安装程序!';
+            return $this->error($msg);
+        }
+
+        return View::fetch();
+    }
+
+    //安装完成
+    public function complete(){
+        clearstatcache();
+        
+        // 写入安装锁定文件
+        $lockFile = ROOT_PATH .'install.lock';
+        $result = @file_put_contents($lockFile, 'lock');
+        //创建配置文件
+        View::assign('info', session('config_file'));
+        session('step', null);
+        session('error', null);
+        session('update',null);
+        return View::fetch();
+    }
+}
