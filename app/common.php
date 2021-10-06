b8fe2d79a7132d2e092c16a5a4a7c77cab7eacc7
dameng <59262424@qq.com>
2021-10-06 19:47:52 +0800

    梳理系统升级方案

--- app/common.php
+++ app/common.php
@@ -272,22 +272,24 @@ if (!function_exists('think_decrypt')) {
     }
 }
 
-/**
- * 数据签名认证
- * @param  array $data 被认证的数据
- * @return string       签名
- * @author 麦当苗儿 <zuojiazi@vip.qq.com>
- */
-function data_auth_sign($data)
-{
-    //数据类型检测
-    if (!is_array($data)) {
-        $data = (array)$data;
+if (!function_exists('data_auth_sign')) {
+    /**
+     * 数据签名认证
+     * @param  array $data 被认证的数据
+     * @return string       签名
+     * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+     */
+    function data_auth_sign($data)
+    {
+        //数据类型检测
+        if (!is_array($data)) {
+            $data = (array)$data;
+        }
+        ksort($data); //排序
+        $code = http_build_query($data); //url编码并生成query字符串
+        $sign = sha1($code); //生成签名
+        return $sign;
     }
-    ksort($data); //排序
-    $code = http_build_query($data); //url编码并生成query字符串
-    $sign = sha1($code); //生成签名
-    return $sign;
 }
 
 /**
