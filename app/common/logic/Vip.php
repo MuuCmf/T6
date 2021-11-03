76aeb41dde06290b4108c8b1ab613a2d60626279
dameng <59262424@qq.com>
2021-11-03 14:14:06 +0800

    新增部分公共业务逻辑层

---
+++ app/common/logic/Vip.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\common\logic;
+
+/*
+ * MuuCmf
+ * 订单数据逻辑层
+ */
+class Vip
+{
+    public $_status  = [
+        '1'  => '启用',
+        '0'  => '禁用',
+        '-1' => '删除',
+    ];
+
+    /**
+     * 格式化数据
+     */
+    public function formatData($data)
+    {   
+
+        
+        return $data;
+    }
+
+
+}
