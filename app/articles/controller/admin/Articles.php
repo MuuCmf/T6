278df03c355ad00eade0be1727a360ef2105a8a0
dameng <59262424@qq.com>
2021-11-28 10:54:00 +0800

    新增文章管理端控制区

---
+++ app/articles/controller/admin/Articles.php
@@ -0,0 +1,163 @@
+<?php
+namespace app\articles\controller\admin;
+
+use think\facade\View;
+use app\articles\model\ArticlesCategory as CategoryModel;
+use app\articles\logic\Category as CategoryLogic;
+use app\articles\model\ArticlesArticles as ArticlesModel;
+use app\articles\logic\Articles as ArticlesLogic;
+use app\common\model\Module;
+use app\articles\validate\Articles as ArticlesValidate;
+use think\exception\ValidateException;
+
+class Articles extends Admin
+{
+    protected $ModuleModel;
+    protected $CategoryModel;
+    protected $CategoryLogic;
+    protected $ArticlesModel;
+    protected $ArticlesLogic;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->ModuleModel = new Module();
+        $this->CategoryModel = new CategoryModel(); //分类模型
+        $this->CategoryLogic = new CategoryLogic(); //分类逻辑
+        $this->ArticlesModel = new ArticlesModel();
+        $this->ArticlesLogic = new ArticlesLogic();
+    }
+
+    /**
+     * 文章列表页
+     */
+    public function lists()
+    {
+        $keyword = input('keyword', '', 'text');
+        View::assign('keyword', $keyword);
+        $category_id = input('category_id', 0, 'intval');
+        View::assign('category_id', $category_id);
+        $status = input('status') == null?'all':input('status');
+        View::assign('status', $status);
+        // 获取查询条件
+        $map = $this->ArticlesLogic->getMap(0, $keyword, $category_id, $status);
+        // 获取列表
+        $lists = $this->ArticlesModel->getListByPage($map, 'sort DESC,id DESC', '*', 20);
+        $pager = $lists->render();
+        $lists = $lists->toArray();
+        
+        foreach($lists['data'] as &$val){
+            $val = $this->ArticlesLogic->formatData($val);
+        }
+        unset($val);
+
+        // ajax请求返回数据
+        if(request()->isAjax()){
+            return $this->success('success', $lists);
+        }
+        View::assign('pager',$pager);
+        View::assign('lists',$lists);
+
+        // 获取分类树
+        $category_list = $this->CategoryModel->getList([['status','=',1]], 999)->toArray();
+        $category_tree = $this->CategoryLogic->categoryTree($category_list);
+        View::assign('category_tree', $category_tree);
+        
+        // 记录当前列表页的cookie
+        Cookie('__forward__', $_SERVER['REQUEST_URI']);
+
+        // 输出模板
+        return View::fetch();
+    }
+
+    /**
+     * 新增、编辑文章
+     */
+    public function edit()
+    {
+        $id = input('id',0,'intval');
+        $title = $id ? "编辑" : "新建";
+        View::assign('title',$title);
+
+        if (request()->isPost()) {
+            $data = input();
+            // 数据验证
+            try {
+                validate(ArticlesValidate::class)->check([
+                    'title'  => $data['title'],
+                    'description' => $data['description'],
+                    'cover' => $data['cover'],
+                    'category_id' => $data['category_id']
+                ]);
+            } catch (ValidateException $e) {
+                // 验证失败 输出错误信息
+                // dump($e->getError());exit;
+                return $this->error($e->getError());
+            }
+
+            $res = $this->ArticlesModel->edit($data);
+
+            if($res){
+                return $this->success($title . '成功', $res, Cookie('__forward__'));
+            }else{
+                return $this->error($title . '失败');
+            }
+        }
+
+        //获取数据
+        $data['id'] = 0;
+        $data['title'] = '';
+        $data['description'] = '';
+        $data['cover'] = '';
+        $data['category_id'] = 0;
+        $data['sort'] = 0; // 排序
+        $data['content'] = '';
+        $data['f_view'] = 0;
+        $data['f_support'] = 0;
+        $data['f_favorites'] = 0;
+        $data['status'] = 0; // 状态
+        $data['reason'] = ''; // 审核拒绝原因
+
+        if(!empty($id)){
+            $data = $this->ArticlesModel->getDataById($id);
+            $data = $this->ArticlesLogic->formatData($data);
+        }
+        View::assign('data',$data);
+
+        // 获取分类树
+        $category_list = $this->CategoryModel->getList([['status','=',1]], 999)->toArray();
+        $category_tree = $this->CategoryLogic->categoryTree($category_list);
+        View::assign('category_tree', $category_tree);
+
+        // 输出模板
+        return View::fetch();
+    }
+
+    /**
+     * 设置状态
+     */
+    public function status()
+    {   
+        $ids = input('ids/a');
+        !is_array($ids)&&$ids=explode(',',$ids);
+        $status = input('status', 0, 'intval');
+        $title = '更新';
+        if($status == 0){
+            $title = '禁用';
+        }
+        if($status == 1){
+            $title = '启用';
+        }
+        if($status == -3){
+            $title = '删除';
+        }
+        $data['status'] = $status;
+
+        $res = $this->ArticlesModel->where('id', 'in', $ids)->update($data);
+        if($res){
+            return $this->success($title . '成功');
+        }else{
+            return $this->error($title . '失败');
+        }  
+    }
+}
