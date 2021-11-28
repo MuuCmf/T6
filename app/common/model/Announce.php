8b35cc855d413ae5c98662f6028450afd4d5ff94
dameng <59262424@qq.com>
2021-11-28 09:59:59 +0800

    公共公告逻辑层重写方法

--- app/common/model/Announce.php
+++ app/common/model/Announce.php
@@ -5,5 +5,12 @@ class Announce extends BaseModel
 {   
     // 开启自动写入时间戳字段
     protected $autoWriteTimestamp = true;
+
+    public function getList($map, $limit=10, $order = 'create_time desc' ,$field = '*')
+    {
+        $list = $this->where($map)->limit($limit)->order($order)->field($field)->select();
+        
+        return $list;
+    }
     
 }
