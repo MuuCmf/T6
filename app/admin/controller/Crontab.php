39ad0611f2c1d76d88f0962020ae4339c7fb0d3e
lix <jxx0410@sina.com>
2022-02-25 14:14:07 +0800

    计划任务管理

---
+++ app/admin/controller/Crontab.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ *                                  |
+ *     __     __  __     __  __     | FILE: Crontab.php
+ *    /\ \   /\_\_\_\   /\_\_\_\    | AUTHOR: 季骁宣
+ *   _\_\ \  \/_/\_\/_  \/_/\_\/_   | EMAIL: jxx0410@sina.com
+ *  /\_____\   /\_\/\_\   /\_\/\_\  | QQ: 516036855
+ *  \/_____/   \/_/\/_/   \/_/\/_/  | DATETIME: 2022/2/24
+ *                                  |-------------------------------------
+ *                                  | 登山则情满于山,观海则意溢于海
+ * +----------------------------------------------------------------------
+ */
+namespace app\admin\controller;
+use app\common\model\Crontab as CrontabModel;
+use app\common\logic\Crontab as CrontabLogic;
+use app\common\model\CrontabLog as CrontabLogModel;
+use think\facade\View;
+
+class Crontab extends Admin{
+    protected $CrontabLogic;
+    protected $CrontabModel;
+    protected $CrontabLogModel;
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->CrontabModel = new CrontabModel();
+        $this->CrontabLogic = new CrontabLogic();
+        $this->CrontabLogModel = new CrontabLogModel();
+    }
+
+    public function list(){
+        $map = [
+            ['status' ,'between' ,[0,1]],
+            ['shopid' ,'=' ,$this->shopid]
+        ];
+        $list = $this->CrontabModel->getListByPage($map,'id DESC','id,title,description,execute,cycle,day,hour,minute,status,update_time');
+        $pager = $list->render();
+        $list = $list->toArray();
+        foreach ($list['data'] as &$item){
+            $item = $this->CrontabLogic->formatData($item);
+        }
+        unset($item);
+        View::assign([
+            'pager' => $pager,
+            'list' => $list['data']
+        ]);
+        return view();
+    }
+
+    public function edit(){
+        if (request()->isPost()){
+            $params = input('post.');
+            $data = [
+                'id'        =>  $params['id'],
+                'shopid'    =>  $this->shopid,
+                'title'     =>  $params['title'],
+                'description'   =>  $params['description'],
+                'execute'   =>  $params['execute'],
+                'cycle'     =>  $params['cycle'],
+                'day'       =>  $params['day'],
+                'hour'      =>  $params['hour'],
+                'minute'    =>  $params['minute'],
+                'status'    =>  $params['status']
+            ];
+            $result = $this->CrontabModel->edit($data);
+            if ($result){
+                $this->success('设置成功','',url('list'));
+            }
+            $this->error('网路异常，请稍后再试');
+        }
+        $id = input('id',0);
+        $data = [];
+        if (!empty($id)){
+            $data = $this->CrontabModel->getDataById($id);
+            if ($data){
+                $data = $data->toArray();
+            }
+        }
+        View::assign([
+            'data' => $data
+        ]);
+        return \view();
+    }
+
+    public function log(){
+        $map = [
+            ['status' ,'between' ,[0,1]],
+            ['shopid' ,'=' ,$this->shopid]
+        ];
+        $list = $this->CrontabModel->getListByPage($map,'id DESC');
+        $pager = $list->render();
+        $list = $list->toArray();
+        unset($item);
+        View::assign([
+            'pager' => $pager,
+            'list' => $list['data']
+        ]);
+        return view();
+    }
+}
