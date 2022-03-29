2181f41ed9cfa8d32a875e3a5eba62bc67141b52
lix <jxx0410@sina.com>
2022-03-29 10:53:00 +0800

    更新api基类

--- app/common/controller/Api.php
+++ app/common/controller/Api.php
@@ -5,11 +5,12 @@ class Api extends Base{
 
     public $shopid;//店铺ID
     public $module;//请求的应用
+    public $params;//参数
     function __construct()
     {
         parent::__construct();
         $this->initModuleName();
-        $params = request()->param();
+        $this->params = request()->param();
         $this->shopid = $params['shopid'] ?? 0;
     }
 
