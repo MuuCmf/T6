a65a9d428f6e4f54d1a8313e0bd0e8e415a644a8
dameng <59262424@qq.com>
2022-05-24 12:17:55 +0800

    移除冗余

--- app/index/gulpfile.js
+++ app/index/gulpfile.js
@@ -13,8 +13,8 @@ var gulp        = require('gulp'),
     reload      = browserSync.reload;
     
 // 定义源代码的目录和编译压缩后的目录
-var src='../src/index',
-    dist='../../static/index';
+var src='./_src/',
+    dist='../../public/static/index';
 // 编译全部scss 并压缩
 gulp.task('scss', function(){
     gulp.src([
@@ -36,7 +36,6 @@ gulp.task('mod_js', function() {
         .pipe(babel({
           presets: ['es2015']
         }))
-        .pipe(concat('knowledge.js'))//合并js
         .pipe(rename({suffix: '.min'}))//rename压缩后的文件名
         .pipe(uglify())
         .pipe(gulp.dest(dist + '/js'));
@@ -66,12 +65,12 @@ gulp.task('server', function() {
     });
     */
     // 监听scss文件编译
-    gulp.watch('css/**/*.scss', ['scss']);
+    gulp.watch(src + 'css/**/*.scss', ['scss']);
     // 监听js文件编译
-    gulp.watch('js/**/*.js', ['mod_js']);
+    gulp.watch(src + 'js/**/*.js', ['mod_js']);
     // 监听其他不编译的文件 有变化直接copy
-    gulp.watch('images/**/*.!(jpg|jpeg|png|gif|bmp|svg)', ['images']);
-    gulp.watch('lib/**/*.!(jpg|jpeg|png|gif|bmp|svg|css|js)', ['lib']);   
+    gulp.watch(src + 'images/**/*.!(jpg|jpeg|png|gif|bmp|svg)', ['images']);
+    gulp.watch(src + 'lib/**/*.!(jpg|jpeg|png|gif|bmp|svg|css|js)', ['lib']);   
     // 监听css文件变化后刷新页面
     gulp.watch('css/*.css').on("change", reload);
 });
