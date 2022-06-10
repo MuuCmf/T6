07e521eb0a00a7c8f9af0ae742965c6d0b88342c
dameng <59262424@qq.com>
2022-06-10 16:01:17 +0800

    调试接口基类

--- app/common/controller/Api.php
+++ app/common/controller/Api.php
@@ -1,9 +1,16 @@
 <?php
 namespace app\common\controller;
 
+use think\facade\Cookie;
+use think\facade\Config;
+use thans\jwt\exception\JWTException;
+use thans\jwt\exception\TokenBlacklistException;
+use thans\jwt\exception\TokenBlacklistGracePeriodException;
+use thans\jwt\exception\TokenExpiredException;
 use thans\jwt\facade\JWTAuth;
 
-class Api extends Base{
+class Api extends Base
+{
 
     public $shopid = 0;//店铺ID
     public $module;//请求的应用
@@ -22,7 +29,8 @@ class Api extends Base{
     /**
      * 实例化应用名称
      */
-    protected function initModuleName(){
+    protected function initModuleName()
+    {
         $this->module = $this->params['app'] ?? App('http')->getName();
     }
 
@@ -32,9 +40,28 @@ class Api extends Base{
     protected function checkToken()
     {
         $header = request()->header();
-        if(isset($header['token'])){
-            $token = $header['token'];
-            $payload = JWTAuth::auth(false);
+        if(isset($header['authorization'])){
+            header('Access-Control-Expose-Headers:Authorization,authorization');//用于暴露response中的token，h5因w3c规范导致获取不到
+            try {
+                $payload = JWTAuth::auth();
+            } catch (TokenExpiredException $e) { // 捕获token过期
+                // 尝试刷新token，会将旧token加入黑名单
+                JWTAuth::setRefresh();
+                $token = JWTAuth::refresh();
+                $payload = JWTAuth::auth(false);
+            } 
+
+            if (isset($token)) {
+                JWTAuth::setToken($token);
+
+                if (in_array('cookie', Config::get('jwt.token_mode'))) {
+                    Cookie::set('token', $token);
+                }
+                if (in_array('header', Config::get('jwt.token_mode'))) {
+                    response()->header(['Authorization' => 'Bearer '.$token]);
+                }
+            }
+            
             $uid = $payload['uid']->getValue();
             $this->uid = $uid;
         }
