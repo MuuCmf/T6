cb5e6730175c27e21768c584a5f8acd6628a4d58
dameng <59262424@qq.com>
2022-08-10 11:04:24 +0800

    优化系统静态首页样式

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -5,32 +5,18 @@ namespace app\index\controller;
 
 use think\facade\View;
 use app\common\controller\Common;
-use app\common\logic\Config;
-use app\channel\facade\wechat\OfficialAccount;
-use app\common\model\Orders as OrdersModel;
-use app\common\model\CapitalFlow;
-
 
 class Index extends Common
 {
     function __construct()
     {
         parent::__construct();
-        $this->OrderModel = new OrdersModel();
-        $this->CapitalFlowModel = new CapitalFlow();
     }
     public function index()
     {
         $this->setTitle('首页');
-        $this->setKeywords('首页,MuuCmf T6');
-        $this->setDescription('首页,MuuCmf T6');
+        $this->setKeywords('MuuCmf T6,开发框架,CMS,Muu云课堂,Muu云小店,Muu云课堂V2,知识付费,知识服务,电商系统,电商软件,');
+        $this->setDescription('MuuCmf T6开源低代码应用开发框架，北京火木科技有限公司 版权所有并提供技术支持');
         return View::fetch();
     }
-
-    public function debug()
-    {
-        dump(config());
-    }
-
-    
 }
