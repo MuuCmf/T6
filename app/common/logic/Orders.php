18522713250513334ce75171687ce25407f8c09b
dameng <59262424@qq.com>
2022-10-19 11:49:51 +0800

    新增订单导出数据处理方法

--- app/common/logic/Orders.php
+++ app/common/logic/Orders.php
@@ -85,4 +85,53 @@ class Orders extends Base
         
         return $data;
     }
+
+    /**
+     * 导出数据格式化
+     */
+    public function exportParse($list, $header = array()){
+        if (empty($list)) {
+            return '';
+        }
+        
+        $keys = array_keys($header);
+        $html = "\xEF\xBB\xBF";
+        foreach ($header as $li) {
+            $html .= $li . "\t ,";
+        }
+        $html .= "\n";
+        $count = count($list);
+        $pagesize = ceil($count/5000);
+        for ($j = 1; $j <= $pagesize; $j++) {
+            $list = array_slice($list, ($j-1) * 5000, 5000);
+            
+            if (!empty($list)) {
+                $size = ceil(count($list) / 500);
+                for ($i = 0; $i < $size; $i++) {
+                    $buffer = array_slice($list, $i * 500, 500);
+                    
+                    $column_data = array();
+                    foreach ($buffer as &$row) {
+                        if($row)
+                        if($row['paid'] == 1){
+                            $row['paid_time'] = date('Y-m-d H:i:s', $row['paid_time']);
+                        }
+                        $row['paid'] = $this->_paid[$row['paid']];
+                        $row['paid_fee'] = '￥' . sprintf("%.2f",$row['paid_fee']/100);
+                        $row['create_time'] = date('Y-m-d H:i:s', $row['create_time']);
+                        $row['products'] = json_decode($row['products'],true);
+                        $row['products_title'] = $row['products']['title'];
+                        foreach ($keys as $key) {
+                            $data[] = $row[$key];
+                        }
+                        $column_data[] = implode("\t ,", $data) . "\t ,";
+                        unset($data);
+                    }
+                    unset($row);
+                    $html .= implode("\n", $column_data) . "\n";
+                }
+            }
+        }
+        return $html;
+    }
 }
