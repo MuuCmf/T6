b8e7daa7a2f17714b42190307c765f5db6c29e22
dameng <59262424@qq.com>
2022-10-26 23:32:51 +0800

    优化创作者页面小功能

--- app/ucenter/controller/Author.php
+++ app/ucenter/controller/Author.php
@@ -13,10 +13,6 @@ class Author extends Common
     protected $AuthorModel;
     protected $AuthorLogic;
 
-    protected $middleware = [
-        'app\\common\\middleware\\CheckAuth' => ['only'=>['follow','isfollow']],
-    ];
-    
     public function __construct()
     {
         parent::__construct();
@@ -77,95 +73,25 @@ class Author extends Common
         }
         View::assign('data', $data);
 
-        $this->setTitle($data['name']);
-        return View::fetch();
-    }
-
-    /**
-     * 关注、取消关注
-     */
-    public function follow()
-    {
-        $author_id = input('author_id', 0, 'intval');
+        // 查询是否已关注
         $uid = get_uid();
-
-        // 判断是否已关注
         $map = [
             ['shopid', '=', $this->shopid],
             ['uid', '=', $uid],
-            ['author_id', '=', $author_id],
-        ];
-        $follow = (new AuthorFollowModel())->where($map)->find();
-
-        // 写入数据初始化
-        $data = [];
-        if($follow){
-            $data['id'] = $follow['id'];
-            if($follow['status'] == 1) $data['status'] = 0;
-            if($follow['status'] == 0) $data['status'] = 1;
-        }else{
-            $data['shopid'] = $this->shopid;
-            $data['uid'] = $uid;
-            $data['author_id'] = $author_id;
-            $data['status'] = 1;
-        }
-
-        $res = (new AuthorFollowModel())->edit($data);
-        if($res){
-            return $this->success($data['status'] ? '已关注':'已取消');
-        }
-
-        return $this->error('发生错误');
-    }
-
-    /**
-     * 是否已关注
-     */
-    public function isfollow()
-    {
-        $author_id = input('author_id', 0, 'intval');
-        $uid = get_uid();
-
-        $map = [
-            ['shopid', '=', $this->shopid],
-            ['uid', '=', $uid],
-            ['author_id', '=', $author_id],
+            ['author_id', '=', $id],
             ['status', '=', 1]
         ];
         $res = (new AuthorFollowModel())->where($map)->find();
+        $has_follow = false;
         if($res){
-            return $this->success('已关注');
+            $has_follow = true;
         }
+        View::assign('has_follow', $has_follow);
 
-        return $this->error('未关注或缺少参数');
-    }
-
-    /**
-     * 关注创作者列表
-     */
-    public function followList()
-    {
-        $uid = get_uid();
-        $rows = input('rows',20, 'intval');
-        $order_field = input('order_field', 'id', 'text');
-        $order_type = input('order_type', 'desc', 'text');
-        $order = $order_field . ' ' . $order_type;
-
-        // 查询条件
-        $map = [
-            ['shopid', '=', $this->shopid],
-            ['uid', '=', $uid],
-            ['status', '=', 1]
-        ];
-        $fields = '*';
-        $lists = (new AuthorFollowModel())->getListByPage($map,$order,$fields, $rows);
-        $lists = $lists->toArray();
-        foreach($lists['data'] as &$val){
-            $val = (new AuthorFollowLogic())->formatData($val);
-        }
-        unset($val);
-        // ajax请求返回数据
-        return $this->success('success', $lists);
+        // 设置页面TITLE
+        $this->setTitle($data['name']);
+        // 输出页面
+        return View::fetch();
     }
 
 }
