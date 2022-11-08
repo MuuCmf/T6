14da797df2ef958b412d5ad492aa4b00689c6ef7
dameng <59262424@qq.com>
2022-11-08 19:05:49 +0800

    优化二维码生成方法

--- app/api/controller/Qrcode.php
+++ app/api/controller/Qrcode.php
@@ -10,6 +10,7 @@ class Qrcode extends Api
      */
     public function create($url){
         $url = str_replace('./','',urldecode($url));
+        ob_clean();//这个一定要加上，清除缓冲区
         $qrcode = qrcode($url,false,false,false,'8','L',2,false);
         echo $qrcode;exit();
     }
