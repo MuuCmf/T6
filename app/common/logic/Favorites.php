f7a77f072a025b4928f8d5c28b8da719d36bd3d0
dameng <59262424@qq.com>
2023-03-19 20:59:14 +0800

    优化用户收藏和浏览历史数据处理逻辑

--- app/common/logic/Favorites.php
+++ app/common/logic/Favorites.php
@@ -1,21 +1,35 @@
 <?php
+
 namespace app\common\logic;
 
 use app\common\model\Module;
-use think\helper\Str;
 
 class Favorites extends Base
 {
-    public function formatData($data){
-        //获取应用名
-        $data['module_name'] =  $data['app'] == 'system' ? '系统' :Module::where('name',$data['app'])->value('alias');
-        $data['user_info'] = query_user($data['uid'],['nickname','avatar']);//用户信息
-        $data['products'] = json_decode($data['metadata'],true);
-        $data['products'] = $this->setImgAttr($data['products'], '1:1');
-        if (isset($data['products']['price'])){
-            $data['products']['price'] = sprintf("%.2f",$data['products']['price']/100);
+    public function formatData($data)
+    {
+        // 约定各应用内容交由应用内部处理
+        // 约定类名 Favorites 约定方法formatData
+        $class_namespace = "\\app\\{$data['app']}\\logic\\Favorites";
+        if (class_exists($class_namespace)) {
+            $appLogic = new $class_namespace;
+            $data = $appLogic->formatData($data);
         }
+
+        if (empty($data['products'])) {
+            $data['products'] = json_decode($data['metadata'], true);
+            $data['products'] = $this->setImgAttr($data['products'], '1:1');
+            if (isset($data['products']['price'])) {
+                $data['products']['price'] = sprintf("%.2f", $data['products']['price'] / 100);
+            }
+        }
+
+        //获取应用名
+        $data['module_name'] =  $data['app'] == 'system' ? '系统' : Module::where('name', $data['app'])->value('alias');
+        $data['user_info'] = query_user($data['uid'], ['nickname', 'avatar']); //用户信息
+
         $data = $this->setTimeAttr($data);
+
         return $data;
     }
 }
