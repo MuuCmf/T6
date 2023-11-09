1fc9f59f0d2c4d6e811d23426c65e2a4df6eac8d
dameng <59262424@qq.com>
2023-11-10 00:25:42 +0800

    完善用户实名认证数据的处理

--- app/common/model/MemberAuthentication.php
+++ app/common/model/MemberAuthentication.php
@@ -4,4 +4,40 @@ namespace app\common\model;
 class MemberAuthentication extends Base
 {
     protected $autoWriteTimestamp = true;
+    protected $_card_type = [
+        0 => '身份证',
+    ];
+    protected $_status  = [
+        0  => '待审核',
+        1  => '已审核',
+        2  => '审核未通过',
+        -1 => '删除',
+    ];
+
+    public function handle($data)
+    {
+        $data['user_info'] = query_user($data['uid']);
+
+        if(isset($data['status'])){
+            $data['status_str'] = $this->_status[$data['status']];
+        }
+
+        if(isset($data['card_type'])){
+            $data['card_type_str'] = $this->_card_type[$data['card_type']];
+        }
+
+        // 图片处理
+        if(isset($data['front'])){
+            $data['front_original'] = get_attachment_src($data['front']);
+        }
+        if(isset($data['back'])){
+            $data['back_original'] = get_attachment_src($data['back']);
+        }
+
+        //时间戳格式化
+        $data['create_time_str'] = time_format($data['create_time']);
+        $data['update_time_str'] = time_format($data['update_time']);
+        
+        return $data;
+    }
 }
