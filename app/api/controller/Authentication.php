46ce04dcdbc3cc610245f6ecbe0289fdc3f933e7
dameng <59262424@qq.com>
2023-11-10 19:25:11 +0800

    新增用户实名验证数据验证类

--- app/api/controller/Authentication.php
+++ app/api/controller/Authentication.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\api\controller;
+
+use think\exception\ValidateException;
 use app\common\controller\Api;
+use app\common\validate\Authentication as AuthenticationValidate;
 use app\common\model\MemberAuthentication as AuthenticationModel;
 
 class Authentication extends Api
@@ -18,7 +21,39 @@ class Authentication extends Api
      */
     public function edit()
     {
+        if (request()->isPost()) {
+            $param = request()->post();
+            $uid = get_uid();
 
+            $data = [
+                'id' => $param['id'],
+                'uid' => $uid,
+                'shopid' => $this->shopid,
+                'name' => $param['name'],
+                'card_type' => $param['card_type'],
+                'card_no' => $param['card_no'],
+                'front' => $param['front'],
+                'back' => $param['back'],
+                'status' => 0, //默认待审核状态
+            ];
+
+            // 数据验证
+            try {
+                validate(AuthenticationValidate::class)->check($data);
+            } catch (ValidateException $e) {
+                // 验证失败 输出错误信息
+                return $this->error($e->getError());
+            }
+
+            //写入数据
+            $res = $this->AuthenticationModel->edit($data);
+            if ($res) {
+                //返回提示
+                return $this->success('提交成功！', $res);
+            } else {
+                return $this->error('提交失败！');
+            }
+        }
     }
 
 }
---
+++ app/common/validate/Authentication.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\common\validate;
+ 
+use think\Validate;
+ 
+/**
+ * 验证器
+ */
+class Authentication extends Validate
+{
+    protected $rule = [
+        'name'  =>  'require|egt:1',
+        'uid' => 'require|egt:1',
+        'card_type'  =>  'require|egt:1',
+        'card_no'  =>  'require|egt:1',
+        'front'  =>  'require',
+        'back'  =>  'require',
+    ];
+    
+    protected $message  =   [
+        'name.require' =>  '姓名不能为空',
+        'name.egt' =>  '姓名不能为空',
+        'uid.require' => '用户ID为空',
+        'uid.egt' => '用户ID为空',
+        'card_type.require' => '证件类型不能为空',
+        'card_type.egt' => '证件类型不能为空',
+        'card_no.require' => '证件号码不能为空',
+        'card_no.egt' => '证件号码不能为空',
+        'front.require' => '证件图片不能为空',
+        'back.require' => '证件图片不能为空',
+    ];
+    
+    protected $scene = [
+        
+    ];
+}
