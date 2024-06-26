cb3dae9bfb07a5d2e4c6fade5d2055d36dccbf5c
大蒙 <59262424@qq.com>
2024-06-26 10:19:47 +0800

    新增创作者绑定用户的自定义唯一性验证

--- app/common/validate/Author.php
+++ app/common/validate/Author.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace app\common\validate;
- 
+
 use think\Validate;
- 
+use app\common\model\Author as AuthorModel;
+
 /**
  * 创作者验证器
  */
@@ -14,9 +16,10 @@ class Author extends Validate
         'cover' => 'require',
         'professional'  =>  'require',
         'group_id' => 'require|gt:0',
+        'uid' => 'require|checkUidUnique',
         'content'  =>  'require',
     ];
-    
+
     protected $message  =   [
         'name.require' =>  '真实姓名不能为空',
         'description.require' => '简短描述不能为空',
@@ -24,11 +27,32 @@ class Author extends Validate
         'professional.require' => '职称不能为空',
         'group_id.require' => '创作者类型未选择',
         'group_id.gt' => '创作者类型未选择',
+        'uid.checkUidUnique' => '该用户已经绑定了其他创作者',
         'content.require' => '详情描述不能为空',
+    ];
 
-    ];
-    
     protected $scene = [
-        'edit'   =>  ['name','description','cover','professional','group_id','content'],
+        'edit'   =>  ['name', 'description', 'cover', 'professional', 'group_id', 'uid', 'content'],
     ];
+
+    protected function checkUidUnique($value, $rule, $data)
+    {
+        if ($value > 0) {
+            $AuthorModel = new AuthorModel();
+            $map = [];
+            $map[] = ['uid', '=', $value];
+            $map[] = ['status', 'in', [0, 1, -1, -2]];
+            if (!empty($data['id'])) {
+                $map[] = ['id', '<>', $data['id']];
+            }
+
+            $count = $AuthorModel->where($map)->count();
+
+            if ($count > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
