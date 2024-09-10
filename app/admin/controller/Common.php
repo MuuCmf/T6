c1e768974bf1798228b67f88d2eb95c092b26fc5
大蒙 <59262424@qq.com>
2024-09-10 10:15:41 +0800

    移除冗余

--- app/admin/controller/Common.php
+++ app/admin/controller/Common.php
@@ -1,49 +1,11 @@
 <?php
 namespace app\admin\controller;
 
-use think\facade\View;
 use think\facade\Cache;
-use thans\jwt\facade\JWTAuth;
-use app\common\controller\Base;
 use app\common\model\Member as CommonMember;
 
-class Common extends Base
+class Common extends Admin
 {
-    /**
-     * 用户登录
-     */
-    public function login()
-    {
-        if (request()->isPost()) {
-            //获取参数
-            $account = input('post.account', '', 'text');
-            $password = input('post.password', '', 'text');
-
-            if(empty($account)){
-                return $this->error('账号不能为空');
-            }
-            // 验证账号和密码
-            $commonMemberModel = new CommonMember;
-            $uid = $commonMemberModel->verifyUserPassword($account, $password);
-            if($uid < 1){
-                return $this->error($commonMemberModel->error);
-            }
-
-            // 登录
-            $res = $commonMemberModel->login(0, $uid);
-
-            if ($res) {
-                $token = JWTAuth::builder(['uid' => $uid]); //参数为用户认证的信息，请自行添加
-                return $this->success('登录成功', $token, url('admin/index/index'));
-            } else {
-                return $this->error($commonMemberModel->error);
-            }
-        }else{
-            // 模板输出
-            return View::fetch();
-        }
-    }
-    
     /* 退出登录 */
     public function logout(){
 
@@ -52,7 +14,7 @@ class Common extends Base
             $commonMemberModel->logout(is_login());
             return $this->success('退出成功','', url('ucenter/common/login'));
         } else {
-            return $this->error('error');
+            return $this->error('已退出登录');
         }
     }
 
