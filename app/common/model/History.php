1c3ddcad2d86c1e830b4fe7bc5600d87ed7361b6
大蒙 <59262424@qq.com>
2024-12-08 12:56:19 +0800

    新增浏览记录模型部分统计方法

--- app/common/model/History.php
+++ app/common/model/History.php
@@ -77,4 +77,162 @@ class History extends Base
         return $res;
     }
 
+    /**
+     * 获取今天浏览数量
+     */
+    public function getTodaCount($shopid = 0, $app = '')
+    {
+        $today_time = dayTime();
+        $map = [
+            ['shopid', '=', $shopid],
+            ['update_time', 'between', [$today_time[0], $today_time[1]]]
+        ];
+        if (!empty($app)) {
+            $map[] = ['app', '=', $app];
+        }
+        $count = $this->where($map)->count();
+
+        return $count;
+    }
+
+    /**
+     * 获取本周浏览数量
+     */
+    public function getWeekCount($shopid = 0, $app = '')
+    {
+        list($start, $end) = weekTime();
+        $map[] = ['update_time', 'between', [$start, $end]];
+        $map[] = ['shopid', '=', $shopid];
+        if (!empty($app)) {
+            $map[] = ['app', '=', $app];
+        }
+        $count = $this->where($map)->count();
+
+        return $count;
+    }
+
+    /**
+     * 获取本月浏览数量
+     */
+    public function getMonthCount($shopid = 0, $app = '')
+    {
+        list($start, $end) = monthTime();
+        $map[] = ['update_time', 'between', [$start, $end]];
+        $map[] = ['shopid', '=', $shopid];
+        if (!empty($app)) {
+            $map[] = ['app', '=', $app];
+        }
+        $count = $this->where($map)->count();
+
+        return $count;
+    }
+
+    /**
+     * 今日24小时播放数据量结构
+     */
+    public function todayTotalJson($shopid = 0, $app = '')
+    {
+        //今日订单数量
+        list($start, $end) = dayTime();
+        $today_total = [];
+        for ($i = 0; $i < 24; $i++) {
+            $date_start = $start + (3600 * $i);
+            $date_end = $start + (3600 * ($i + 1));
+            $today_total['time'][$i] = ($i) . ':00-' . ($i + 1) . ':00';
+            $map = [];
+            $map[] = ['update_time', 'between', [$date_start, $date_end]];
+            $map[] = ['shopid', '=', $shopid];
+            if (!empty($app)) {
+                $map[] = ['app', '=', $app];
+            }
+            $count = $this->where($map)->count();
+
+            $today_total['count'][$i] = $count;
+        }
+        $today_total = json_encode($today_total); //今日24小时数据
+
+        return $today_total;
+    }
+
+    /**
+     * 本周每日浏览数据量结构
+     */
+    public function weekTotalJson($shopid = 0, $app = '')
+    {
+        //本周
+        $start = strtotime("this week monday");
+        $week_total = [];
+        for ($i = 0; $i < 7; $i++) {
+            if($i == 0){
+                $date_start = $start;
+                $date_end = $start + 86400;
+            }else{
+                $date_start = $start + 86400 * $i;
+                $date_end = $start + 86400 * ($i + 1);
+            }
+            $week_total['time'][$i] = '周' . ($i + 1);
+            $map = [];
+            $map[] = ['update_time', 'between', [$date_start, $date_end]];
+            $map[] = ['shopid', '=', $shopid];
+            if (!empty($app)) {
+                $map[] = ['app', '=', $app];
+            }
+            $count = $this->where($map)->count();
+            $week_total['count'][$i] = $count;
+        }
+        $week_total = json_encode($week_total); //本周每日数据
+
+        return $week_total;
+    }
+
+    /**
+     * 本月每日浏览数据量结构
+     */
+    public function monthTotalJson($shopid = 0, $app = '')
+    {
+        list($start, $end) = monthTime();
+        $month_total = [];
+        for ($i = 0; $i < ($end + 1 - $start) / 86400; $i++) {
+            $date_start = $start + 86400 * $i;
+            $date_end = $start + 86400 * ($i + 1);
+            $month_total['time'][$i] = ($i + 1) . '日';
+            $map = [];
+            $map[] = ['update_time', 'between', [$date_start, $date_end]];
+            $map[] = ['shopid', '=', $shopid];
+            if (!empty($app)) {
+                $map[] = ['app', '=', $app];
+            }
+            $count = $this->where($map)->count();
+            $month_total['count'][$i] = $count;
+        }
+        $month_total = json_encode($month_total); //本月每日数据
+
+        return $month_total;
+    }
+
+    /**
+     * 本年每月浏览数据量结构
+     */
+    public function yearTotalJson($shopid = 0, $app = '')
+    {
+        $year = date('Y', time());
+        $year_total = [];
+        for ($i = 0; $i < 12; $i++) {
+            $date_start = strtotime($year . '-' . intval($i + 1) . " first day of");
+            $date_end = strtotime($year . '-' . intval($i + 1) . " last day of") + 86400;
+            $year_total['time'][$i] = ($i + 1) . '月';
+            $map = [];
+            $map[] = ['update_time', 'between', [$date_start, $date_end]];
+            $map[] = ['shopid', '=', $shopid];
+            if (!empty($app)) {
+                $map[] = ['app', '=', $app];
+            }
+            $count = $this->where($map)->count();
+            $year_total['count'][$i] = $count;
+        }
+        $year_total = json_encode($year_total); //本年每月数据
+
+        return $year_total;
+    }
+
 }
