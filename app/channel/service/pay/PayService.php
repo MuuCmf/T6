67babd01c21f59c455890bed730d6cc135e40b26
大蒙 <59262424@qq.com>
2025-04-11 22:50:45 +0800

    优化语法规范

--- app/channel/service/pay/PayService.php
+++ app/channel/service/pay/PayService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\channel\service\pay;
 
 abstract class PayService
@@ -6,15 +7,17 @@ abstract class PayService
     protected $app;
     public $config;
     public $type;
-    public $sandbox;//沙箱模式
-    public $module;//模块
-    public $shopid;//店铺Id
-    public $platform;//平台
+    public $sandbox; //沙箱模式
+    public $module; //模块
+    public $shopid; //店铺Id
+    public $platform; //平台
+    protected $separator;
+
     public function __construct($app)
     {
         $this->separator = DIRECTORY_SEPARATOR;
         $this->app = $app;
-        $this->sandbox = false;//开启沙箱模式
+        $this->sandbox = false; //开启沙箱模式
     }
     abstract function pay($data);
     abstract function refund($order);
