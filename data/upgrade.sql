5cd348f6eb06cdda05e8c55477586d07bdaf5f69
大蒙 <59262424@qq.com>
2025-12-05 20:44:28 +0800

    优化会员卡项数据表部分数据类型

--- data/upgrade.sql
+++ data/upgrade.sql
@@ -287,3 +287,5 @@ ALTER TABLE `muucmf_orders` ADD `agreed_start_time` INT(11) UNSIGNED NOT NULL DE
 ALTER TABLE `muucmf_orders` ADD `agreed_end_time` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT '约定结束时间戳' AFTER `agreed_start_time`;
 
 ALTER TABLE `muucmf_vip_card` CHANGE `app` `app` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '应用唯一标识';
+
+ALTER TABLE `muucmf_vip_card` CHANGE `category_ids` `category_ids` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '支持的分类IDS';
