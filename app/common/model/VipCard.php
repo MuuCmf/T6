fea84a22cb636f9829bdefb33817e07b0e23fa5f
大蒙 <59262424@qq.com>
2025-12-20 22:39:17 +0800

    同下

--- app/common/model/VipCard.php
+++ app/common/model/VipCard.php
@@ -90,6 +90,98 @@ class VipCard extends Base
         //获取用户未到期的所有会员卡
         $vipModel = new Vip();
         $where = "v.`shopid`={$shopid} and vc.`app` like '%{$app}%' and v.`uid`={$uid} and (v.`end_time` > " . time() . " or v.`end_time`=0) and v.`status`=1 and vc.`status`=1";
+        $vip_list = $vipModel->alias('v')
+        ->join('vip_card vc', 'vc.id = v.card_id')
+        ->whereRaw($where)
+        ->field('v.*, vc.id vip_card_id, vc.app vip_card_app, vc.status vip_card_status')
+        ->select();
+
+        if (empty($vip_list)) {
+            return null;
+        }else{
+            $vip_list = $vip_list->toArray();
+            $vipLogic = new VipLogic();
+            foreach ($vip_list as &$v) {
+                unset($v['vip_card_id']);
+                unset($v['vip_card_app']);
+                unset($v['vip_card_status']);
+                $v = $vipLogic->formatData($v);
+            }
+            unset($v);
+        }
+
+        //获取商品数据
+        $file_name = ucfirst($app) . ucfirst($product_type);
+        $namespace = "app\\{$app}\\model\\{$file_name}";
+        $productModel = new $namespace;
+        $product_data = $productModel->where('id', $product_id)->find();
+        $product_data = $product_data->toArray();
+        
+        if (!empty($product_data)) {
+            //循环查询会员卡是否支持该产品，删除不支持的会员卡元素
+            foreach ($vip_list as $key => &$val) {
+
+                if(is_json($val['vip_card_info']['category_ids'])){
+                    // 多应用数据处理
+                    if (!$this->getVipCardSupportAppCategoryId($val['vip_card_info'], $app, $product_data['category_id'])) {
+                        unset($vip_list[$key]);
+                    } else {
+                        //根据折扣计算该会员价格
+                        $member_price = intval($product_data['price'] * ($val['vip_card_info']['discount'] / 10));
+                        $val['member_price'] = sprintf("%.2f", floatval($member_price / 100));
+                    }
+                }else{
+                    // 单应用数据处理
+                    if (!in_array($product_data['category_id'], $val['vip_card_info']['category_ids_arr'])) {
+                        unset($vip_list[$key]);
+                    } else {
+                        //根据折扣计算该会员价格
+                        $member_price = intval($product_data['price'] * ($val['vip_card_info']['discount'] / 10));
+                        $val['member_price'] = sprintf("%.2f", floatval($member_price / 100));
+                    }
+                }
+            }
+            unset($val);
+            
+            //查询优惠力度最大的会员卡
+            $resule = [];
+            if (!empty($vip_list) && count($vip_list) >= 1) {
+                $discount_arr = [];
+                foreach ($vip_list as $key => $val) {
+                    if(!empty($val['vip_card_info'])){
+                        $discount_arr[$key] = $val['vip_card_info']['discount'];
+                    }
+                }
+                asort($discount_arr);
+                $key = key($discount_arr);
+                $resule = $vip_list[$key];
+            }
+            
+            return $resule;
+        }
+        return null;
+    }
+
+    /**
+     * 获取用户可用的最优会员卡
+     *
+     * 1. 查询用户未过期的所有有效会员卡
+     * 2. 根据商品类别筛选支持的会员卡
+     * 3. 计算会员卡折扣后的商品价格
+     * 4. 返回优惠力度最大的会员卡信息
+     *
+     * @param int $shopid 店铺ID
+     * @param string $app 应用名称
+     * @param int $uid 用户ID
+     * @param int $product_id 商品ID
+     * @param string $product_type 商品类型
+     * @return array|null 返回最优会员卡信息，无可用卡返回null
+     */
+    public function getUserAbleOptimalCard(int $shopid, string $app, int $uid, int $product_id, string $product_type)
+    {
+        //获取用户未到期的所有会员卡
+        $vipModel = new Vip();
+        $where = "v.`shopid`={$shopid} and vc.`app` like '%{$app}%' and v.`uid`={$uid} and (v.`end_time` > " . time() . " or v.`end_time`=0) and v.`status`=1 and vc.`status`=1";
         $vip_card_list = $vipModel->alias('v')
         ->join('vip_card vc', 'vc.id = v.card_id')
         ->whereRaw($where)
@@ -130,7 +222,7 @@ class VipCard extends Base
                     } else {
                         //根据折扣计算该会员价格
                         $member_price = intval($product_data['price'] * ($val['discount'] / 10));
-                        $val['member_price'] = sprintf("%.2f", $member_price / 100);
+                        $val['member_price'] = sprintf("%.2f", floatval($member_price / 100));
                     }
                 }else{
                     // 单应用数据处理
@@ -139,7 +231,7 @@ class VipCard extends Base
                     } else {
                         //根据折扣计算该会员价格
                         $member_price = intval($product_data['price'] * ($val['discount'] / 10));
-                        $val['member_price'] = sprintf("%.2f", $member_price / 100);
+                        $val['member_price'] = sprintf("%.2f", floatval($member_price / 100));
                     }
                 }
             }
