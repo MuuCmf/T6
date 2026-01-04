c5752d0bb3d6c92a98a461042657d783ba144a51
大蒙 <59262424@qq.com>
2026-01-04 23:43:23 +0800

    优化webpack配置项

--- webpack.modules.config.js
+++ webpack.modules.config.js
@@ -107,9 +107,21 @@ function getAllModules() {
     return [];
   }
 
-  return fs.readdirSync(appDir, { withFileTypes: true })
+  const allModules = fs.readdirSync(appDir, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name);
+
+  // 核心模块列表，无论是否有 webpack.config.js 都保留
+  const coreModules = ['admin', 'common', 'channel', 'ucenter', 'index'];
+
+  // 过滤模块：保留核心模块，其他模块只保留有 webpack.config.js 的
+  return allModules.filter(moduleName => {
+    if (coreModules.includes(moduleName)) {
+      return true;
+    }
+    const webpackConfigPath = path.join(appDir, moduleName, 'webpack.config.js');
+    return fs.existsSync(webpackConfigPath);
+  });
 }
 
 module.exports = {
