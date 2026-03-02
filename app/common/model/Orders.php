1dfc0be560043cd9d60c029a0ae28e6115641187
大蒙 <59262424@qq.com>
2026-03-02 11:43:10 +0800

    优化订单数据统计方法

--- app/common/model/Orders.php
+++ app/common/model/Orders.php
@@ -41,7 +41,7 @@ class Orders extends Base
         //判断内容是否购买
         $res = $this->whereRaw(
             $where,
-            ['shopid' => intval($shopid), 'app' => $app, 'id' => intval($id), 'uid' => intval($uid), 'type' => $type, 'time' => intval(time())]
+            ['shopid' => intval($shopid), 'app' => $app, 'id' => intval($id), 'type' => $type, 'uid' => intval($uid), 'time' => intval(time())]
         )->findOrEmpty();
 
         if (!$res->isEmpty()) {
@@ -54,7 +54,7 @@ class Orders extends Base
     /**
      * 统计订单销售金额数据
      */
-    public function total($shopid = 'all', $app = '')
+    public function total($shopid = 'all', $app = '', $order_info_type = '')
     {
         //累计收入
         if ($shopid != 'all') {
@@ -64,17 +64,27 @@ class Orders extends Base
         //已支付
         $total_all_map[] = ['paid', '=', 1];
         //关联应用
-        $total_all_map[] = ['app', '=', $app];
+        if(!empty($app)){
+            $total_all_map[] = ['app', '=', $app];
+        }
+        if(!empty($order_info_type)){
+            $total_all_map[] = ['order_info_type', '=', $order_info_type];
+        }
         $total_all_sum = $this->where($total_all_map)->sum('paid_fee');
         $total_all_sum = sprintf("%.2f", $total_all_sum / 100);
 
         //本月收入
         $total_month_map = [
-            ['app', '=', $app],
             ['pay_channel', 'not in', ['score', 'password']],
             ['paid', '=', 1],
             ['paid_time', 'between', [monthTime()[0], monthTime()[1]]],
         ];
+        if(!empty($app)){
+            $total_month_map[] = ['app', '=', $app];
+        }
+        if(!empty($order_info_type)){
+            $total_month_map[] = ['order_info_type', '=', $order_info_type];
+        }
         if ($shopid != 'all') {
             $total_month_map[] = ['shopid', '=', $shopid];
         }
@@ -83,7 +93,6 @@ class Orders extends Base
 
         //本周收入
         $total_week_map = [
-            ['app', '=', $app],
             ['pay_channel', 'not in', ['score', 'password']],
             ['paid', '=', 1],
             ['paid_time', 'between', [weekTime()[0], weekTime()[1]]],
@@ -91,18 +100,29 @@ class Orders extends Base
         if ($shopid != 'all') {
             $total_week_map[] = ['shopid', '=', $shopid];
         }
+        if(!empty($app)){
+            $total_week_map[] = ['app', '=', $app];
+        }
+        if(!empty($order_info_type)){
+            $total_week_map[] = ['order_info_type', '=', $order_info_type];
+        }
         $total_week_sum = $this->where($total_week_map)->sum('paid_fee');
         $total_week_sum = sprintf("%.2f", $total_week_sum / 100);
 
         //本日收入
         $total_today_map = [
-            ['app', '=', $app],
             ['pay_channel', 'not in', ['score', 'password']],
             ['paid', '=', 1],
             ['paid_time', 'between', [dayTime()[0], dayTime()[1]]],
         ];
         if ($shopid != 'all') {
             $total_today_map[] = ['shopid', '=', $shopid];
+        }
+        if(!empty($app)){
+            $total_today_map[] = ['app', '=', $app];
+        }
+        if(!empty($order_info_type)){
+            $total_today_map[] = ['order_info_type', '=', $order_info_type];
         }
         $total_today_sum = $this->where($total_today_map)->sum('paid_fee');
         $total_today_sum = sprintf("%.2f", $total_today_sum / 100);
@@ -182,7 +202,7 @@ class Orders extends Base
     /**
      * 获取今天订单数量
      */
-    public function getTodayOrdersCount($shopid = 0, $app = '')
+    public function getTodayOrdersCount($shopid = 0, $app = '', $order_info_type = '')
     {
         $today_time = dayTime();
         $map = [
@@ -192,6 +212,10 @@ class Orders extends Base
         if (!empty($app)) {
             $map[] = ['app', '=', $app];
         }
+        if (!empty($order_info_type)) {
+            $map[] = ['order_info_type', '=', $order_info_type];
+        }
+        
         $count = $this->where($map)->count();
         return $count;
     }
@@ -199,7 +223,7 @@ class Orders extends Base
     /**
      * 获取今天总收入
      */
-    public function getTodayPaidFeeSum($shopid = 0, $app = '')
+    public function getTodayPaidFeeSum($shopid = 0, $app = '', $order_info_type = '')
     {
         $today_time = dayTime();
         $map = [
@@ -210,6 +234,9 @@ class Orders extends Base
         if (!empty($app)) {
             $map[] = ['app', '=', $app];
         }
+        if (!empty($order_info_type)) {
+            $map[] = ['order_info_type', '=', $order_info_type];
+        }
         $data = $this->where($map)->sum('paid_fee');
         $data = sprintf("%.2f", $data / 100);
 
@@ -219,7 +246,7 @@ class Orders extends Base
     /**
      * 获取本周订单总收入
      */
-    public function getWeekPaidFeeSum($shopid = 0, $app = '')
+    public function getWeekPaidFeeSum($shopid = 0, $app = '', $order_info_type = '')
     {
         list($start, $end) = weekTime();
         $map[] = ['create_time', 'between', [$start, $end]];
@@ -228,6 +255,9 @@ class Orders extends Base
         $map[] = ['pay_channel', 'not in', ['score', 'password']];
         if (!empty($app)) {
             $map[] = ['app', '=', $app];
+        }
+        if (!empty($order_info_type)) {
+            $map[] = ['order_info_type', '=', $order_info_type];
         }
         $data = $this->where($map)->sum('paid_fee');
         $data = sprintf("%.2f", $data / 100);
@@ -238,7 +268,7 @@ class Orders extends Base
     /**
      * 获取本月订单总收入
      */
-    public function getMonthPaidFeeSum($shopid = 0, $app = '')
+    public function getMonthPaidFeeSum($shopid = 0, $app = '', $order_info_type = '')
     {
         list($start, $end) = monthTime();
         $map[] = ['create_time', 'between', [$start, $end]];
@@ -248,6 +278,9 @@ class Orders extends Base
         if (!empty($app)) {
             $map[] = ['app', '=', $app];
         }
+        if (!empty($order_info_type)) {
+            $map[] = ['order_info_type', '=', $order_info_type];
+        }
         $data = $this->where($map)->sum('paid_fee');
         $data = sprintf("%.2f", $data / 100);
         return $data;
@@ -256,7 +289,7 @@ class Orders extends Base
     /**
      * 获取总订单数量
      */
-    public function getAllOrdersCount($shopid = 0, $app = '')
+    public function getAllOrdersCount($shopid = 0, $app = '', $order_info_type = '')
     {
         $where = [
             ['shopid', '=', $shopid],
@@ -265,6 +298,9 @@ class Orders extends Base
         if (!empty($app)) {
             $where[] = ['app', '=', $app];
         }
+        if (!empty($order_info_type)) {
+            $where[] = ['order_info_type', '=', $order_info_type];
+        }
         $count = $this->where($where)->count();
         return $count;
     }
@@ -272,7 +308,7 @@ class Orders extends Base
     /**
      * 获取总订单总收入
      */
-    public function getAllPaidFeeSum($shopid = 0, $app = '')
+    public function getAllPaidFeeSum($shopid = 0, $app = '', $order_info_type = '')
     {
         $where = [
             ['shopid', '=', $shopid],
@@ -282,6 +318,9 @@ class Orders extends Base
         if (!empty($app)) {
             $where[] = ['app', '=', $app];
         }
+        if (!empty($order_info_type)) {
+            $where[] = ['order_info_type', '=', $order_info_type];
+        }
         $data = $this->where($where)->sum('paid_fee');
         $data = sprintf("%.2f", $data / 100);
         return $data;
@@ -290,7 +329,7 @@ class Orders extends Base
     /**
      * chart需要的今日24小时订单数据量结构
      */
-    public function todayTotalJson($shopid = 0, $app = '')
+    public function todayTotalJson($shopid = 0, $app = '', $order_info_type = '')
     {
         //今日订单数量
         list($start, $end) = dayTime();
@@ -306,6 +345,9 @@ class Orders extends Base
             if (!empty($app)) {
                 $map[] = ['app', '=', $app];
             }
+            if (!empty($order_info_type)) {
+                $map[] = ['order_info_type', '=', $order_info_type];
+            }
             $count = $this->where($map)->count();
             $today_total['count'][$i] = $count;
         }
@@ -317,7 +359,7 @@ class Orders extends Base
     /**
      * chart需要的本周每日订单数据量结构
      */
-    public function weekTotalJson($shopid = 0, $app = '')
+    public function weekTotalJson($shopid = 0, $app = '', $order_info_type = '')
     {
         //本周
         $start = strtotime("this week monday");
@@ -338,6 +380,9 @@ class Orders extends Base
             if (!empty($app)) {
                 $map[] = ['app', '=', $app];
             }
+            if (!empty($order_info_type)) {
+                $map[] = ['order_info_type', '=', $order_info_type];
+            }
             $count = $this->where($map)->count();
             $week_total['count'][$i] = $count;
         }
@@ -349,7 +394,7 @@ class Orders extends Base
     /**
      * chart需要的本月每日订单数据量结构
      */
-    public function monthTotalJson($shopid = 0, $app = '')
+    public function monthTotalJson($shopid = 0, $app = '', $order_info_type = '')
     {
         list($start, $end) = monthTime();
         $month_total = [];
@@ -364,6 +409,9 @@ class Orders extends Base
             if (!empty($app)) {
                 $map[] = ['app', '=', $app];
             }
+            if (!empty($order_info_type)) {
+                $map[] = ['order_info_type', '=', $order_info_type];
+            }
             $count = $this->where($map)->count();
             $month_total['count'][$i] = $count;
         }
@@ -375,7 +423,7 @@ class Orders extends Base
     /**
      * chart需要的本年每月订单数据量结构
      */
-    public function yearTotalJson($shopid = 0, $app = '')
+    public function yearTotalJson($shopid = 0, $app = '', $order_info_type = '')
     {
         $year = date('Y', time());
         $year_total = [];
@@ -389,6 +437,9 @@ class Orders extends Base
             $map[] = ['shopid', '=', $shopid];
             if (!empty($app)) {
                 $map[] = ['app', '=', $app];
+            }
+            if (!empty($order_info_type)) {
+                $map[] = ['order_info_type', '=', $order_info_type];
             }
             $count = $this->where($map)->count();
             $year_total['count'][$i] = $count;
