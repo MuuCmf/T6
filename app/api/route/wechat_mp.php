b82f722c1c483fd4dbd00e6038a9014a1090d5a6
大蒙 <59262424@qq.com>
2026-03-14 21:09:05 +0800

    补充部分路由配置

--- app/api/route/wechat_mp.php
+++ app/api/route/wechat_mp.php
@@ -3,7 +3,11 @@
 use think\facade\Route;
 
 Route::group('wechat_mp', function () {
+    Route::get('config', 'api/WechatMiniProgram/config');
     Route::get('code', 'api/WechatMiniProgram/code');
     Route::post('login', 'api/WechatMiniProgram/login');
+    Route::get('unlimitQrcode', 'api/WechatMiniProgram/unlimitQrcode');
     Route::post('bindMobile', 'api/WechatMiniProgram/bindMobile');
+    Route::get('toMiniProgramLists', 'api/WechatMiniProgram/toMiniProgramLists');
+    Route::get('toMiniProgramDetail', 'api/WechatMiniProgram/toMiniProgramDetail');
 });
--- app/api/route/wechat_work.php
+++ app/api/route/wechat_work.php
@@ -5,4 +5,7 @@ use think\facade\Route;
 Route::group('wechat_work', function () {
     Route::get('config', 'api/WechatWork/config');
     Route::any('callback', 'api/WechatWork/callback');
+    Route::get('oauth', 'api/WechatWork/oauth');
+    Route::post('jssdk', 'api/WechatWork/jssdk');
+    Route::any('oauth_callback', 'api/WechatWork/oauthCallback');
 });
