28f4cfee6d3aeb7734edd1a5143398c329585c15
大蒙 <59262424@qq.com>
2026-03-19 18:42:07 +0800

    优化文章列表接口传参

--- app/articles/controller/api/Articles.php
+++ app/articles/controller/api/Articles.php
@@ -24,11 +24,16 @@ class Articles extends Base
         $keyword = input('keyword', '', 'text');
         $category_id = input('category_id', 0, 'intval');
         $rows = input('rows', 20, 'intval');
+        // 约束rows范围
+        $rows = min($rows, 100);
+        $order_field = input('order_field', 'id', 'text');
+        $order_type = input('order_type', 'desc', 'text');
+        $order = 'sort DESC,' . $order_field . ' ' . $order_type;
 
         // 获取查询条件
         $map = $this->ArticlesLogic->getMap($this->shopid, $keyword, $category_id, 1);
         // 获取列表
-        $lists = $this->ArticlesModel->getListByPage($map, 'sort DESC,id DESC', '*', $rows);
+        $lists = $this->ArticlesModel->getListByPage($map, $order, '*', $rows);
         $lists = $lists->toArray();
         
         foreach($lists['data'] as &$val){
