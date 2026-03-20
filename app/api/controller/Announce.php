d9d90f7395099a6ea5ab79a0a716ce83d7ada362
大蒙 <59262424@qq.com>
2026-03-21 00:48:06 +0800

    优化公告列表接口

--- app/api/controller/Announce.php
+++ app/api/controller/Announce.php
@@ -25,10 +25,14 @@ class Announce extends Api
      * @throws \think\db\exception\DataNotFoundException
      * @throws \think\db\exception\ModelNotFoundException
      * @throws \think\exception\DbException
+     * 
+     * @api
+     * @param int $id 公告ID
+     * @success-json {"code":200,"msg":"获取成功！","data":{}}
      */
     public function detail()
     {
-        $id = input('get.id', 0);
+        $id = input('id', 0, 'intval');
         $data = $this->AnnounceModel->getDataById($id);
         $data = $this->AnnounceLogic->formatData($data);
 
@@ -45,11 +49,16 @@ class Announce extends Api
      * 
      * @api
      * @param string $teminal 终端标识（可选）
-     * @success-json {"code":1,"msg":"获取成功！","data":[...]}
+     * @success-json {"code":200,"msg":"获取成功！","data":[...]}
      */
     public function lists()
     {
         $teminal = input('teminal', '', 'text');
+        $rows = input('rows', 3, 'intval');
+
+        // rows限制
+        $rows = min($rows, 10);
+        
         //初始化查询条件
         $map = [
             ['shopid', '=', $this->shopid],
@@ -59,7 +68,46 @@ class Announce extends Api
             $map[] = ['teminal', '=', $teminal];
         }
         
-        $lists = $this->AnnounceModel->getList($map, 5, 'sort desc, create_time desc');
+        $lists = $this->AnnounceModel->getList($map, $rows, 'sort desc, create_time desc');
+        foreach ($lists as &$item) {
+            $item = $this->AnnounceLogic->formatData($item);
+        }
+        unset($item);
+
+        return $this->success('获取成功！', $lists);
+    }
+
+    /**
+     * 获取公告分页列表
+     * 
+     * @return \think\response\Json 返回公告分页列表数据
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     * 
+     * @api
+     * @param string $teminal 终端标识（可选）
+     * @success-json {"code":200,"msg":"获取成功！","data":[...]}
+     */
+    public function pageLists()
+    {
+        $teminal = input('teminal', '', 'text');
+        $rows = input('rows', 10, 'intval');
+
+        // rows限制
+        $rows = min($rows, 10);
+        
+        //初始化查询条件
+        $map = [
+            ['shopid', '=', $this->shopid],
+            ['status', '=', 1]
+        ];
+        if(!empty($teminal)){
+            $map[] = ['teminal', '=', $teminal];
+        }
+        
+        $lists = $this->AnnounceModel->getListByPage($map, 'sort desc, create_time desc', '*', $rows);
+        
         foreach ($lists as &$item) {
             $item = $this->AnnounceLogic->formatData($item);
         }
