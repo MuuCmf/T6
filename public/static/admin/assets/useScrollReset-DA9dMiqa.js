c8c395c8d990a45cd2a276b56b5987d56f5aa8d0
大蒙 <59262424@qq.com>
2026-03-23 13:34:45 +0800

    修复中后台前端已知问题

--- public/static/admin/assets/useScrollReset-DA9dMiqa.js
+++
@@ -1,1 +0,0 @@
-import{r as o,n as r}from"./vue-vendor-BvKLiYyq.js";function e(){const e=o(null);return{scrollContainerRef:e,resetScrollTop:()=>{r(()=>{e.value&&(e.value.scrollTop=0)})}}}export{e as u};
---
+++ public/static/admin/assets/useScrollReset.js
@@ -0,0 +1,1 @@
+import{r as o,n as r}from"./vue-vendor.js";function e(){const e=o(null);return{scrollContainerRef:e,resetScrollTop:()=>{r(()=>{e.value&&(e.value.scrollTop=0)})}}}export{e as u};
