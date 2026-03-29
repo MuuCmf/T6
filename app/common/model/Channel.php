7809280a3f10dfd394c95c28feb767d943ebf384
大蒙 <59262424@qq.com>
2026-03-29 10:15:48 +0800

    修复自定义导航选项为应用时数据处理

--- app/common/model/Channel.php
+++ app/common/model/Channel.php
@@ -10,13 +10,23 @@ class Channel extends Model
 {
     /**
      * 获取导航列表，支持多级导航
+     * @param  string $block 导航类型
      * @param  boolean $field 要列出的字段
      * @return array          导航树
      */
     public function lists($block = 'navbar', $field = true)
     {
         $map[] = ['block', '=', $block];
-        $list = $this->field($field)->where($map)->order('sort')->select()->toArray();
+        $list = $this->field($field)->where($map)->order('sort')->select();
+        if ($list) {
+            $list = $list->toArray();
+
+            foreach ($list as $key => $value) {
+                if($value['type'] == 'app'){
+                    $list[$key]['url'] = $value['app'];
+                }
+            }
+        }
         
         return $list;
     }
